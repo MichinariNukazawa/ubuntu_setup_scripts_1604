d8d56b7ec283559cd0a71f6b9c41645138f8527b
MichinariNukazawa <michinari.nukazawa@gmail.com>
2018-02-26 00:58:57 +0900

    move to 16.04 scripts and default editor to vi/vim

--- 16.04/config.sh
+++ 16.04/config.sh
@@ -6,7 +6,14 @@ set -o pipefail
 trap 'echo "error:$0($LINENO) \"$BASH_COMMAND\" \"$@\""' ERR
 
 
+# unity 時計
 gsettings set com.canonical.indicator.datetime time-format "custom"
 gsettings set com.canonical.indicator.datetime custom-time-format "'%x (%a) %H:%M'"
 
+
+# vi(vim)タブ入力補完
+#set -o vi
+
+# git エディタをvimへ切り替え
+git config --global core.editor 'vim -c "set fenc=utf-8"'
 
