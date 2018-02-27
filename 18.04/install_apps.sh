11c02642d8a1a0c872d925bff759e4a8a95fc73e
MichinariNukazawa <michinari.nukazawa@gmail.com>
2018-02-27 12:08:16 +0900

    dropbox

--- 18.04/install_apps.sh
+++ 18.04/install_apps.sh
@@ -1,5 +1,18 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+set -x
+
+trap 'echo "error:$0($LINENO) \"$BASH_COMMAND\" \"$@\""' ERR
 
 # apps
-sudo apt install mdadm samba squid gimp chromium-browser thunderbird git revelation vlc synaptic gjiten gparted vim -y
+sudo apt update
+sudo apt install \
+	mdadm samba squid gimp chromium-browser thunderbird \
+	git revelation vlc synaptic gjiten gparted vim \
+	nautilus-dropbox \
+	-y
+# skypeforlinux
 # fcitx-mozc
 
