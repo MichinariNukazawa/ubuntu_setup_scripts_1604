8d00ec7b85282bd09b2e174d5d5686d90a1aa677
MichinariNukazawa <michinari.nukazawa@gmail.com>
2019-10-25 21:06:15 +0900

    remove app squid

--- 18.04/install_apps.sh
+++ 18.04/install_apps.sh
@@ -9,8 +9,10 @@ trap 'echo "error:$0($LINENO) \"$BASH_COMMAND\" \"$@\""' ERR
 # apps
 sudo apt update
 sudo apt install \
-	mdadm samba squid gimp chromium-browser thunderbird \
-	git revelation vlc synaptic gjiten gparted vim \
+	mdadm samba \
+	gimp chromium-browser thunderbird \
+	git vlc synaptic gjiten \
+	gparted vim \
 	nautilus-dropbox \
 	-y
 # skypeforlinux
